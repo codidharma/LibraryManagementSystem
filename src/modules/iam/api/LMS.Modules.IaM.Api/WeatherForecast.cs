fea290c8151bc23d953a2e0238cd41059c390638
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-02-28 20:33:14 +0530

    Added Identity Access Management Module

---
+++ src/modules/iam/api/LMS.Modules.IaM.Api/WeatherForecast.cs
@@ -0,0 +1,10 @@
+// Configure the HTTP request pipeline.
+
+
+namespace LMS.Modules.IaM.Api;
+
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
