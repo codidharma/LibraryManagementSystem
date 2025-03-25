c9566b6725744e048e806f72c4454ab2ed8f6478
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-03-25 23:23:59 +0530

    Added shaed api project

---
+++ src/hosting/LMS.API/Program.cs
@@ -0,0 +1,26 @@
+using LMS.ServiceDefaults;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+WebApplication app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+await app.RunAsync();
+
--- src/hosting/LMS.AppHost/Program.cs
+++ src/hosting/LMS.AppHost/Program.cs
@@ -1,3 +1,5 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.LMS_API>("lms-api");
+
 await builder.Build().RunAsync();
