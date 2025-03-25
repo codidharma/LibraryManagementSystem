24d851d62851d6f05bd48d4c8750777acd136ddf
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-03-25 21:33:05 +0530

    Added infrastructure tests for entity configurations

---
+++ src/modules/membership/tests/LMS.Modules.Membership.ArchitectureTests/InfrastructureTests/EntityConfigurationTests.cs
@@ -0,0 +1,43 @@
+﻿using LMS.Modules.Membership.ArchitectureTests.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Modules.Membership.ArchitectureTests.InfrastructureTests;
+
+public class EntityConfigurationTests : TestBase
+{
+    [Fact]
+    public void EntityConfigurations_ShouldBe_NonPublicSealedClasses()
+    {
+        //Act
+        TestResult result = Types
+            .InAssembly(InfrastructureAssembly)
+            .That()
+            .Inherit(typeof(IEntityTypeConfiguration<>))
+            .Should()
+            .BeClasses()
+            .And()
+            .BeSealed()
+            .And()
+            .NotBePublic()
+            .GetResult();
+
+        //Assert
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void EntityConfigurations_ShouldHave_ConfigurationPostFix()
+    {
+        //Act
+        TestResult result = Types
+            .InAssembly(InfrastructureAssembly)
+            .That()
+            .Inherit(typeof(IEntityTypeConfiguration<>))
+            .Should()
+            .NotHaveNameEndingWith("Configuration")
+            .GetResult();
+
+        //Assert
+        Assert.True(result.IsSuccessful);
+    }
+}
