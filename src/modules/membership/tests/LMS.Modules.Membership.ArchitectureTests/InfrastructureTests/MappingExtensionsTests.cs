0f490369f0b7ef735b2d90ae036e069e473e4f8f
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-03-25 13:06:19 +0530

    Added architecture tests for dao mapping extensions

---
+++ src/modules/membership/tests/LMS.Modules.Membership.ArchitectureTests/InfrastructureTests/MappingExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using LMS.Modules.Membership.ArchitectureTests.Base;
+
+namespace LMS.Modules.Membership.ArchitectureTests.InfrastructureTests;
+
+public class MappingExtensionsTests : TestBase
+{
+    private const string MappingExtensionsNamespacePrefix = "LMS.Modules.Membership.Infrastructure.Mapping";
+
+    [Fact]
+    public void MappingExtensions_ShouldBe_NonPublicStaticClasses()
+    {
+        //Act
+        TestResult result = Types
+            .InAssembly(InfrastructureAssembly)
+            .That()
+            .ResideInNamespaceStartingWith(MappingExtensionsNamespacePrefix)
+            .Should()
+            .BeClasses()
+            .And()
+            .BeStatic()
+            .And()
+            .NotBePublic()
+            .GetResult();
+
+        //Assert
+        Assert.True(result.IsSuccessful);
+    }
+
+    [Fact]
+    public void MappingExtenions_ShouldEndWith_ExtensionsPostfix()
+    {
+        //Act
+        TestResult result = Types
+            .InAssembly(InfrastructureAssembly)
+            .That()
+            .ResideInNamespaceStartingWith(MappingExtensionsNamespacePrefix)
+            .Should()
+            .HaveNameEndingWith("Extensions")
+            .GetResult();
+
+        //Assert
+        Assert.True(result.IsSuccessful);
+    }
+}
