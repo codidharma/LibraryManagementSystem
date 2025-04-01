ca50a596d8be712cbb18aa318d8812b52ff51c19
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-04-02 02:27:37 +0530

    Renamed the assembly refrence cs file in Membershp.Api as the CI pipeline is not working.

--- src/modules/membership/src/LMS.Modules.Membership.API/AssemblyReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-namespace LMS.Modules.Membership.Api;
-public static class AssemblyReference
-{
-    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-}
-
---
+++ src/modules/membership/src/LMS.Modules.Membership.Api/ApiAssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace LMS.Modules.Membership.Api;
+public static class ApiAssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(ApiAssemblyReference).Assembly;
+}
+
