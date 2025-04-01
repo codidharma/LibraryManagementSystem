4399565a9779452c55a88f2723473991c55c758d
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-04-02 02:23:25 +0530

    Amended the test project files to check for corrupted refrences in ci build

--- src/modules/membership/src/LMS.Modules.Membership.API/AssemblyReference.cs
+++ src/modules/membership/src/LMS.Modules.Membership.API/AssemblyReference.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 namespace LMS.Modules.Membership.Api;
-
 public static class AssemblyReference
 {
     public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
 }
+
