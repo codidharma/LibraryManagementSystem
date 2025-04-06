d1ba7b3e95469bc23af49e247e06c34f37ade34e
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-04-07 01:17:23 +0530

    Added Result class and associated tests

---
+++ src/common/src/LMS.Common.Domain/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace LMS.Common.Domain;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    public Result(bool isSuccess, Error error)
+    {
+        if (isSuccess && error != Error.None)
+        {
+            throw new InvalidOperationException("ErrorType should be None for success result.");
+        }
+        if (!isSuccess && error == Error.None)
+        {
+            throw new InvalidOperationException("ErrorType should not be None for failure result.");
+        }
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Success() => new(true, Error.None);
+    public static Result Failure(Error error) => new(false, error);
+}
