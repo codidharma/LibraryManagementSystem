7f7b47cfdec2365f9274aed442a39e6d8d160711
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-04-30 11:42:14 +0530

    Added the problemfactory for generating problem details

---
+++ src/common/src/LMS.Common.Api/Results/ProblemFactory.cs
@@ -0,0 +1,68 @@
+﻿using LMS.Common.Domain;
+using Microsoft.AspNetCore.Http;
+
+namespace LMS.Common.Api.Results;
+
+public static class ProblemFactory
+{
+    public static IResult Create(Result result)
+    {
+        if (result.IsSuccess)
+        {
+            //Do nthing
+        }
+        return Microsoft.AspNetCore.Http.Results.Problem(
+            title: GetTitle(result.Error),
+            detail: GetDetail(result.Error),
+            statusCode: GetStatusCode(result.Error),
+            type: GetType(result.Error),
+            extensions: GetExtensions(result)
+            );
+
+        static string GetTitle(Error error) =>
+            error.ErrorType switch
+            {
+                ErrorType.Validation => error.Code,
+                ErrorType.NotFound => error.Code,
+                ErrorType.Conflict => error.Code,
+                _ => "Internal Server Error"
+            };
+        static string GetDetail(Error error) =>
+            error.ErrorType switch
+            {
+                ErrorType.Validation => error.Description,
+                ErrorType.NotFound => error.Description,
+                ErrorType.Conflict => error.Description,
+                _ => "An unexpected error occured. Please contact system administrator."
+            };
+
+        static int GetStatusCode(Error error) =>
+            error.ErrorType switch
+            {
+                ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.NotFound => StatusCodes.Status404NotFound,
+                ErrorType.Conflict => StatusCodes.Status409Conflict,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+        static string GetType(Error error) =>
+            error.ErrorType switch
+            {
+                ErrorType.Validation => "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                ErrorType.NotFound => "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                ErrorType.Conflict => "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+                _ => "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+            };
+        static Dictionary<string, object?>? GetExtensions(Result result)
+        {
+            if (result.Error is not ValidationError ve)
+            {
+                return null;
+            }
+            return new Dictionary<string, object?>
+            {
+                { "errors", ve.Errors}
+            };
+        }
+    }
+}
---
+++ src/common/tests/LMS.Common.Api.Tests/ProblemDetailsTests.cs
@@ -0,0 +1,121 @@
+﻿using LMS.Common.Api.Results;
+using LMS.Common.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+namespace LMS.Common.Api.Tests;
+
+public class ProblemDetailsTests
+{
+    [Fact]
+    public void ForValidationError_Create_ShouldReturn_ProblemDetailsInsatnce()
+    {
+        //Arrange
+        string code = "Generic.Validation";
+        string description = "One or more validation error occured.";
+        int statusCode = StatusCodes.Status400BadRequest;
+        string type = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+        List<Error> errors =
+            [
+                Error.InvalidDomain("Test.InvalidDomainValue", "Value provided was incorrect.")
+            ];
+        Dictionary<string, object?> extensions = [];
+        extensions.Add("errors", errors);
+
+        ValidationError validationError = new(errors);
+        Result validationErrorResult = Result.Failure(validationError);
+
+        //Act
+        ProblemHttpResult problemDetailsHttpResult = (ProblemHttpResult)ProblemFactory.Create(validationErrorResult);
+        ProblemDetails problemDetails = problemDetailsHttpResult.ProblemDetails;
+
+        //Assert
+        Assert.NotNull(problemDetailsHttpResult);
+        Assert.NotNull(problemDetails);
+        Assert.Equal(code, problemDetails.Title);
+        Assert.Equal(description, problemDetails.Detail);
+        Assert.Equal(statusCode, problemDetails.Status);
+        Assert.Equal(type, problemDetails.Type);
+        Assert.Equal(extensions, problemDetails.Extensions);
+    }
+
+    [Fact]
+    public void ForUnspecifiedError_Create_ShouldReturn_ProblemDetailsInsatnce()
+    {
+        //Arrange
+        string code = "Internal Server Error";
+        string description = "An unexpected error occured. Please contact system administrator.";
+        int statusCode = StatusCodes.Status500InternalServerError;
+        string type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+
+        Error error = new Error(code, description, ErrorType.Failure);
+        Result validationErrorResult = Result.Failure(error);
+
+        //Act
+        ProblemHttpResult problemDetailsHttpResult = (ProblemHttpResult)ProblemFactory.Create(validationErrorResult);
+        ProblemDetails problemDetails = problemDetailsHttpResult.ProblemDetails;
+
+        //Assert
+        Assert.NotNull(problemDetailsHttpResult);
+        Assert.NotNull(problemDetails);
+        Assert.Equal(code, problemDetails.Title);
+        Assert.Equal(description, problemDetails.Detail);
+        Assert.Equal(statusCode, problemDetails.Status);
+        Assert.Equal(type, problemDetails.Type);
+        Assert.Empty(problemDetails.Extensions);
+    }
+
+    [Fact]
+    public void ForNotFoundError_Create_ShouldReturn_ProblemDetailsInsatnce()
+    {
+        //Arrange
+        string code = "Common.NotFound";
+        string dummyId = Guid.NewGuid().ToString();
+        string description = $"Unable to find the Common type instance for id {dummyId}";
+        int statusCode = StatusCodes.Status404NotFound;
+        string type = "https://tools.ietf.org/html/rfc7231#section-6.5.4";
+
+        Error error = new Error(code, description, ErrorType.NotFound);
+        Result validationErrorResult = Result.Failure(error);
+
+        //Act
+        ProblemHttpResult problemDetailsHttpResult = (ProblemHttpResult)ProblemFactory.Create(validationErrorResult);
+        ProblemDetails problemDetails = problemDetailsHttpResult.ProblemDetails;
+
+        //Assert
+        Assert.NotNull(problemDetailsHttpResult);
+        Assert.NotNull(problemDetails);
+        Assert.Equal(code, problemDetails.Title);
+        Assert.Equal(description, problemDetails.Detail);
+        Assert.Equal(statusCode, problemDetails.Status);
+        Assert.Equal(type, problemDetails.Type);
+        Assert.Empty(problemDetails.Extensions);
+    }
+
+    [Fact]
+    public void ForConflictError_Create_ShouldReturn_ProblemDetailsInsatnce()
+    {
+        //Arrange
+        string code = "Common.Conflict";
+        string dummyId = Guid.NewGuid().ToString();
+        string description = $"Common type instance for id {dummyId} already exist";
+        int statusCode = StatusCodes.Status409Conflict;
+        string type = "https://tools.ietf.org/html/rfc7231#section-6.5.8";
+
+        Error error = new Error(code, description, ErrorType.Conflict);
+        Result validationErrorResult = Result.Failure(error);
+
+        //Act
+        ProblemHttpResult problemDetailsHttpResult = (ProblemHttpResult)ProblemFactory.Create(validationErrorResult);
+        ProblemDetails problemDetails = problemDetailsHttpResult.ProblemDetails;
+
+        //Assert
+        Assert.NotNull(problemDetailsHttpResult);
+        Assert.NotNull(problemDetails);
+        Assert.Equal(code, problemDetails.Title);
+        Assert.Equal(description, problemDetails.Detail);
+        Assert.Equal(statusCode, problemDetails.Status);
+        Assert.Equal(type, problemDetails.Type);
+        Assert.Empty(problemDetails.Extensions);
+    }
+}
